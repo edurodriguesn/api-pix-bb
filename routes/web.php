21f4b6a7827aa222c968d9595dd1132d1f20b4c3
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-07-03 11:42:21 -0300

    feat: adicionada a rota de gerar pix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PixController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('pix')->group(function () {
+    Route::post('/gerar', [PixController::class, 'store']);
 });
