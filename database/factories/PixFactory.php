2c8a88806a5755acf137370f7a3688a69b1174cd
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-07-04 11:36:13 -0300

    feat: adicionado factory de pix

--- database/factories/PixFactory.php
+++ database/factories/PixFactory.php
@@ -2,9 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use App\Models\Pix;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PixFactory extends Factory
 {
